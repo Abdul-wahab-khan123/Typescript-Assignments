9afa73d5a0eb86c80f176ca6a67039836778a2b9
Abdul-wahab-khan123 <162675151+Abdul-wahab-khan123@users.noreply.github.com>
2024-03-11 00:51:23 +0500

    Add files via upload

---
+++ 12.Greetings.ts
@@ -0,0 +1,9 @@
+let names :string[] =['saad','Ali','usman','Mateen',] ;
+
+const message : string = 'Welcom to Typescrip Class: '
+
+
+for(let i=0; i<names.length;  i++){
+
+    console.log(message + names[i]);
+}
