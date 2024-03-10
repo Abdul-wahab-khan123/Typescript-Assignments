16cee20a24eb163de92758075119deb291b6b607
Abdul-wahab-khan123 <162675151+Abdul-wahab-khan123@users.noreply.github.com>
2024-03-11 00:54:18 +0500

    Add files via upload

---
+++ 14.Guest_list.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_list:string[] = ['Imran','Salman','Abdul Basit'];
+
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you to our party.\n\nThank you')
+
+}
