a7b2fc95b4e207bfb4e1011cd9ec64413963ebc1
Abdul-wahab-khan123 <162675151+Abdul-wahab-khan123@users.noreply.github.com>
2024-03-18 00:54:02 +0500

    Add files via upload

---
+++ 21.TypeScript_Object.ts
@@ -0,0 +1,21 @@
+
+//Write a program that creates Objects containing these items.
+
+// let person_Name :string = "Abdulwahab"
+
+interface person {
+    age : number,
+    name : string,
+    nationality : string,
+    Student : Boolean
+}
+
+let person :person = {
+
+    age : 17,
+    name : 'Abdul wahab',
+    nationality :'Pakistan',
+    Student : true
+
+}
+console.log(person);
