d075083066283ac3b2e519a2f023e7e213678b24
Abdul-wahab-khan123 <162675151+Abdul-wahab-khan123@users.noreply.github.com>
2024-03-29 11:47:50 -0700

    Add files via upload

---
+++ 44.Sandwiches.ts
@@ -0,0 +1,17 @@
+
+function makeSandwish (itmes: string[]){
+    console.log('Making your standwish with:');
+
+    itmes.forEach(element => console.log("-" +element));
+
+    console.log('Enjoy your sandwish !');
+
+}
+
+
+makeSandwish(['Ham','Cheese', 'Lettuce']);
+
+makeSandwish(['Turkey', 'Bacon']);
+
+makeSandwish(['Peanut butter', 'Jelly']);
+
