29f8faf78a418e5df76a0eebabde7c0f4b51d325
Abdul-wahab-khan123 <162675151+Abdul-wahab-khan123@users.noreply.github.com>
2024-03-29 11:42:37 -0700

    Add files via upload

---
+++ 35.Animals.ts
@@ -0,0 +1,8 @@
+
+
+let animals : string[] =['Cat','Dog','Cow'];
+for(let animal of animals){
+    console.log(`A ${animal} would make a great pet.`);
+}
+
+console.log(`Any of these animal would make a great pet!`)
